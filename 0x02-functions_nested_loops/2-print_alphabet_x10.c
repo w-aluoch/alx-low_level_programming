aac17ee573e1105b4c8b45feb561f4e6de42bb97
w-aluoch <wendyaluoch96@gmail.com>
2023-09-12 09:12:26 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make the alphabet
+ * Return: (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	}
+	i++;
+}
+
+
