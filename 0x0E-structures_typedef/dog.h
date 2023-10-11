5c1276c972544f562850044d0f94b0fd8d75f81f
w-aluoch <wendyaluoch96@gmail.com>
2023-10-11 07:24:00 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,5 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
 
+#endif
